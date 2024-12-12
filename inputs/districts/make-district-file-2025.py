4e2bbb1e336ebd731afe68804f3c8ec118f0c599
Eric Dietrich <edietrich@montanafreepress.org>
2024-12-11 17:27:08 -0700

    Add partially complete districts-2025.json pipeline

--- inputs/districts/make-district-file-2025.py
+++ inputs/districts/make-district-file-2025.py
@@ -3,17 +3,38 @@
 # Hoping to set this up in a way that's easy to adapt heading into 2027
 
 import math
+import json
 import pandas as pd
 
-districts = pd.read_csv('./inputs/districts/2024-to-2033-district-geographies.csv')
+def write_json(dict, path):
+    with open(path, 'w') as f:
+        json.dump(dict, f, indent=4)
 
-districts_object = districts.to_dict(orient='records')
+districts = pd.read_csv('./inputs/districts/2024-to-2033-district-geographies.csv').to_dict(orient='records')
+
+# Election history data
+# Need multiple years here because half the Senate is elected to four-year terms
+
+statewide_general_2024 = pd.read_csv('./inputs/districts/election-results/2024-general-statewide-vote-counts.csv')
+# lege_primary_2024 = 
+# lege_general_2024 = 
+# lege_primary_2022 =
+# lege_general_2022 =
+
+def pase_statewide_votes (df, race):
+    return # TK
+    
+def parse_legislative_votes(df, district):
+    return # TK
 
 last_primary_elex_results = {}
 last_general_elex_results = {}
 
 '''
-"pri_elex": {
+# Aiming for this election result form
+# will need to do some logic to determine which year's primary and general elex
+# to pull from for senate districts based on whether they were in cycle in 2022
+        "pri_elex": {
             "leg": [
                 {
                     "name": "STEVE GUNDERSON",
@@ -61,8 +82,7 @@ last_general_elex_results = {}
 '''
 
 output = []
-print(districts_object[0].keys())
-for d in districts_object:
+for d in districts:
 
     number = int(d['district'].replace('HD ','').replace('SD ',''))
     type = d['district'][:2].lower() # "hd" or "sd"
@@ -90,9 +110,9 @@ for d in districts_object:
         "counties": d['counties'], # Counties covered by the district 
 
         # Do we really need these?
-        # "area": 380.868835,
-        # "pop_2010": 9838,
-        # "ai_pop_2010": 264,
+        "area": None,
+        "pop_2010": None,
+        "ai_pop_2010": None,
 
         "last_election": '2024' if (d['in_cycle_2024_election'] == 'yes') else '2022',
         "pri_elex": last_primary_elex_results,
@@ -101,3 +121,4 @@ for d in districts_object:
         
     },)
 
+write_json(output, './inputs/districts/districts-2025.json')
