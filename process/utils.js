ebd19fd39afb89c20fdbf3884d8e3cab4df6ea32
Jacob Olness <jolness1@gmail.com>
2025-01-07 15:04:21 -0700

    rate limited, need to add token

---
+++ process/utils.js
@@ -0,0 +1,17 @@
+import { sync as globSync } from 'glob'
+import fs from 'fs';
+
+
+export const getJson = (path) => JSON.parse(fs.readFileSync(path))
+
+export const collectJsons = (glob_path) => {
+    const files = globSync(glob_path)
+    return files.map(getJson)
+}
+
+export const writeJson = (path, data) => {
+    fs.writeFile(path, JSON.stringify(data, null, 4), function (err) {
+        if (err) throw err;
+        console.log('    JSON written to', path);
+    });
+}
