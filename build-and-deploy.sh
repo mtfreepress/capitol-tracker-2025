21b92112244bc1f0e4942969e4fc9f00c1ba2ca6
Jacob Olness <jolness1@gmail.com>
2025-01-17 12:11:08 -0700

    oops fixed deploy script

--- build-and-deploy.sh
+++ build-and-deploy.sh
@@ -3,4 +3,4 @@ npm run build
 
 # Deploy
 aws s3 sync build s3://projects.montanafreepress.org/capitol-tracker-2025 --delete
-aws cloudfront create-invalidation --distribution-id E3LVPS3XLJHLL5 --paths "/capitol-tracker-2025/*"
+aws cloudfront create-invalidation --distribution-id E1G7ISX2SZFY34 --paths "/capitol-tracker-2025/*"
