3fde0aab0b4079fb1863e7502abdaf0a012b504c
Eric Dietrich <edietrich@montanafreepress.org>
2025-01-27 12:55:46 -0700

    Update footer menu items  + refactor CSS

--- src/design/Footer.js
+++ src/design/Footer.js
@@ -1,7 +1,7 @@
 import { css } from '@emotion/react'
 import Image from 'next/image'
 
-import { footerMenus, footerLogoUrl } from '../config'
+import { footerMenus, footerLogoUrl } from '../config/config.js'
 
 const footerStyle = css`
 
@@ -16,87 +16,95 @@ const footerStyle = css`
     @media (min-width: 782px) {
         padding-top: 2em;
     }
-`
-const footerWrapperStyle = css`
-    max-width: 1200px;
-    margin: auto;
-`
-const footerColumnsStyle = css`
-    text-transform: uppercase;
-    overflow-wrap: break-word;
-
-    display: flex;
-    justify-content: space-between;
-    flex-wrap: wrap;
-
-    margin-left: 39px;
-    margin-right: 39px;
-    max-width: 90%;
-`
-
-const menuStyle = css`
-    margin-right: 40px;
-`
-
-const menuListStyle = css`
-    list-style-image: none;
-    list-style-type: none;
-    list-style-position: outside;
-    padding-inline-start: 0px;
-    font-weight: 400px;
-    letter-spacing: 2px;
-    line-height: 20.48px;
-
-    li {
-        margin-bottom: 12.8px;
+    
+    .footer-wrapper {
+        max-width: 1200px;
+        margin: auto;
     }
+    
+    .footer-column {
+        text-transform: uppercase;
+        overflow-wrap: break-word;
 
-    a {
-        color: #fff;
-        text-decoration: none;
+        display: flex;
+        justify-content: space-between;
+        flex-wrap: wrap;
+
+        margin-left: 39px;
+        margin-right: 39px;
+        max-width: 90%;
     }
-`
+    
+    .footer-menu {
+        margin-right: 40px;
+    }
+    .footer-menu-item {
+        list-style-image: none;
+        list-style-type: none;
+        list-style-position: outside;
+        padding-inline-start: 0px;
+        font-weight: 400px;
+        letter-spacing: 2px;
+        line-height: 20.48px;
 
-const footerInfoStyle = css`
-    display: flex;
-    justify-content: space-between;
-    align-items: flex-end;
-    flex-wrap: wrap;
+        li {
+            margin-bottom: 12.8px;
+        }
 
-    padding-bottom: 20px;
-    margin-left: 39px;
-    margin-right: 39px;
-    color: #aa986a;
+        a {
+            color: #fff;
+            text-decoration: none;
+        }
 
-    a {
+    }
+    .menu-item-list {
+        display: flex;
+        justify-content: space-between;
+        align-items: flex-end;
+        flex-wrap: wrap;
+        
+    }
+    .footer-info {
+        display: flex;
+        justify-content: space-between;
+        align-items: flex-end;
+        flex-wrap: wrap;
+
+        padding-bottom: 20px;
+        margin-left: 39px;
+        margin-right: 39px;
         color: #aa986a;
-        text-decoration: none;
+
+        a {
+            color: #aa986a;
+            text-decoration: none;
+        }
     }
-`
-const footerImgStyle = css`
-    max-width: 100%;
-    height: auto;
+    .footer-image {
+        max-width: 100%;
+        height: auto;
+    }
 `
 
 const Footer = (props) => {
     const menusRendered = footerMenus.map((menu, i) => {
         const itemsRendered = menu.items.map((item, i) => <li key={String(i)}><a href={item.url}>{item.label}</a></li>)
-        return <section css={menuStyle} key={String(i)}>
+        return <section className="footer-menu" key={String(i)}>
             <h2>{menu.label}</h2>
-            <ul css={menuListStyle}>{itemsRendered}</ul>
+            <ul class="footer-menu-item">{itemsRendered}</ul>
         </section>
     })
 
     return <footer css={footerStyle}>
-        <div css={footerWrapperStyle}>
-            <div css={footerColumnsStyle}>
+        <div className="footer-wrapper">
+            <div className="footer-column">
                 {menusRendered}
             </div>
 
-            <div css={footerInfoStyle}>
+            <div className="footer-info">
                 <span>© {new Date().getFullYear()} Montana Free Press. </span>
                 <a href="https://montanafreepress.org/about-mtfp/privacy-policy/">Privacy Policy</a>
-                <Image alt="MTFP logo" width={121} height={47} css={footerImgStyle} src={footerLogoUrl} />
+                <Image alt="MTFP logo" width={121} height={47} className="footer-image" src={footerLogoUrl} />
             </div>
         </div>
 
