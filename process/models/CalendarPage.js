d9bb1b0e0d12320232f5ab0263dd499f87846f5e
Jacob Olness <jolness1@gmail.com>
2025-02-13 12:31:41 -0700

    still broken date comparison logic

--- process/models/CalendarPage.js
+++ process/models/CalendarPage.js
@@ -1,9 +1,10 @@
-import { dateParse } from '../functions.js'
+import { dateParse, dateFormat } from '../functions.js'
 
 export default class CalendarPage {
     constructor({ actions, updateTime, calendarAnnotations }) {
-        const beginningOfToday = new Date(updateTime).setUTCHours(7, 0, 0, 0) // 7 accounts for Montana vs GMT time
-        const formattedUpdateTime = new Date(beginningOfToday).toLocaleDateString('en-US'); // Format to MM/DD/YYYY
+        const beginningOfToday = new Date(updateTime).setUTCHours(7, 0, 0, 0); // 7 accounts for Montana vs GMT time
+        const formattedBeginningOfToday = dateFormat(new Date(beginningOfToday)); // Format to MM/DD/YYYY
+        console.log({ formattedBeginningOfToday });
 
         // For checking that server is handling dates the same as my local machine
         // console.log({
@@ -13,7 +14,7 @@ export default class CalendarPage {
         //     compare: dateParse('01/11/2023') >= beginningOfToday,
         // })
         // console.log(actions)
-        console.log({formattedUpdateTime})
+        // console.log({formattedUpdateTime})
         // const todayOrLaterActions = actions.filter(d => dateParse(d.data.date) >= beginningOfToday)
         const todayOrLaterActions = actions.filter(d => {
             const parsedDate = dateParse(d.data.date);
@@ -21,20 +22,17 @@ export default class CalendarPage {
             return parsedDate >= beginningOfToday;
         });
         console.log('Today or Later Actions:', todayOrLaterActions);
-        const firstTodayAction = todayOrLaterActions[0]
-        console.log({firstTodayAction})
-        // console.log({todayOrLaterActions})
-        const scheduledHearings = todayOrLaterActions.filter(d => d.data.committeeHearingTime)
-        // console.log({scheduledHearings})
-        const scheduledFloorDebates = todayOrLaterActions.filter(d => d.data.scheduledForFloorDebate)
-        // console.log({scheduledFloorDebates})
-        const scheduledFinalVotes = todayOrLaterActions.filter(d => d.data.scheduledForFinalVote)
-        // console.log({scheduledFinalVotes})
-        const datesOnCalendar = Array.from(new Set(scheduledHearings.concat(scheduledFloorDebates).concat(scheduledFinalVotes).map(d => d.data.date)))
-            .sort((a, b) => dateParse(a) - dateParse(b))
 
-        // list of bills used to merge in bill data via graphql query on the frontend
-        const billsOnCalendar = Array.from(new Set([...scheduledHearings, ...scheduledFloorDebates, ...scheduledFinalVotes].map(d => d.data.bill)))
+        const firstTodayAction = todayOrLaterActions[0];
+        console.log({ firstTodayAction });
+
+        const scheduledHearings = todayOrLaterActions.filter(d => d.data.committeeHearingTime);
+        const scheduledFloorDebates = todayOrLaterActions.filter(d => d.data.scheduledForFloorDebate);
+        const scheduledFinalVotes = todayOrLaterActions.filter(d => d.data.scheduledForFinalVote);
+        const datesOnCalendar = Array.from(new Set(scheduledHearings.concat(scheduledFloorDebates).concat(scheduledFinalVotes).map(d => d.data.date)))
+            .sort((a, b) => dateParse(a) - dateParse(b));
+
+        const billsOnCalendar = Array.from(new Set([...scheduledHearings, ...scheduledFloorDebates, ...scheduledFinalVotes].map(d => d.data.bill)));
 
         this.data = {
             datesOnCalendar,
@@ -43,57 +41,8 @@ export default class CalendarPage {
             scheduledFloorDebates,
             scheduledFinalVotes,
             calendarAnnotations,
-        }
+        };
     }
-    export = () => ({ ...this.data })
 
+    export = () => ({ ...this.data });
 }
-
-
-
-// import { dateParse } from '../functions.js'
-
-// export default class CalendarPage {
-//     constructor({ actions, updateTime, calendarAnnotations }) {
-//         const beginningOfToday = new Date(updateTime).setUTCHours(7, 0, 0, 0); // 7 accounts for Montana vs GMT time
-//         const formattedUpdateTime = new Date(beginningOfToday).toLocaleDateString('en-US'); // Format to MM/DD/YYYY
-//         // console.log('Update Time:', updateTime);
-//         // console.log('Beginning of Today:', new Date(beginningOfToday));
-//         // console.log('Formatted Update Time:', formattedUpdateTime);
-//         // For checking that server is handling dates the same as my local machine
-//         // console.log({
-//         //     updateTime,
-//         //     beginningOfToday: new Date(beginningOfToday),
-//         //     parseCheck: new Date(dateParse('01/11/2023')),
-//         //     compare: dateParse('01/11/2023') >= beginningOfToday,
-//         // })
-//         // console.log({actions})
-//         const todayOrLaterActions = actions.filter(d => {
-//             const parsedDate = dateParse(d.date);
-//             const formattedParsedDate = new Date(parsedDate).toLocaleDateString('en-US');
-//             // console.log('Parsed Date:', formattedParsedDate, '>=', formattedUpdateTime, formattedParsedDate >= formattedUpdateTime);
-//             return formattedParsedDate >= formattedUpdateTime;
-//         });
-//         // console.log('Today or Later Actions:', todayOrLaterActions);
-
-//         const scheduledHearings = todayOrLaterActions.filter(d => d.committeeHearingTime);
-//         // console.log('Scheduled Hearings:', scheduledHearings);
-//         const scheduledFloorDebates = todayOrLaterActions.filter(d => d.scheduledForFloorDebate);
-//         const scheduledFinalVotes = todayOrLaterActions.filter(d => d.scheduledForFinalVote);
-//         const datesOnCalendar = Array.from(new Set(scheduledHearings.concat(scheduledFloorDebates).concat(scheduledFinalVotes).map(d => d.date)))
-//             .sort((a, b) => dateParse(a) - dateParse(b));
-
-//         // list of bills used to merge in bill data via graphql query on the frontend
-//         const billsOnCalendar = Array.from(new Set([...scheduledHearings, ...scheduledFloorDebates, ...scheduledFinalVotes].map(d => d.bill)));
-
-//         this.data = {
-//             datesOnCalendar,
-//             billsOnCalendar,
-//             scheduledHearings,
-//             scheduledFloorDebates,
-//             scheduledFinalVotes,
-//             calendarAnnotations,
-//         }
-//     }
-//     export = () => ({ ...this.data })
-// }
