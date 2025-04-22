56546d04a460e7db3628609b458e8083a3e37d39
Jacob Olness <jolness1@gmail.com>
2025-04-22 10:25:21 -0600

    deploy changes

--- build-and-deploy.sh
+++ build-and-deploy.sh
@@ -1,6 +1,32 @@
+
+# Possible improvement:
 # Build
 npm run build
 
-# Deploy
-aws s3 sync build s3://projects.montanafreepress.org/capitol-tracker-2025 --delete --exact-timestamps
+# Deploy HTML/JS/CSS files (excludes PDFs)
+aws s3 sync build s3://projects.montanafreepress.org/capitol-tracker-2025 \
+  --delete \
+  --exact-timestamps \
+  --exclude "*.pdf"
+
+# Deploy PDFs only if they don't exist or if they've changed
+aws s3 sync build s3://projects.montanafreepress.org/capitol-tracker-2025 \
+  --exact-timestamps \
+  --size-only \
+  --include "*.pdf"
+
 aws cloudfront create-invalidation --distribution-id E1G7ISX2SZFY34 --paths "/capitol-tracker-2025/*"
+
+# Selectively invalidate CloudFront
+# aws cloudfront create-invalidation --distribution-id E1G7ISX2SZFY34 --paths "/capitol-tracker-2025/index.html" "/capitol-tracker-2025/_next/*"
+
+
+# Previous version:
+
+
+# # Build
+# npm run build
+
+# # Deploy
+# aws s3 sync build s3://projects.montanafreepress.org/capitol-tracker-2025 --delete --exact-timestamps --size-only --include "*.pdf" --max-concurrent-requests 10
+# aws cloudfront create-invalidation --distribution-id E1G7ISX2SZFY34 --paths "/capitol-tracker-2025/*"
