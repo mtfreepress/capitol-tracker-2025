296329320fca259cb477abfbc61204b21e2b7c80
Jacob Olness <jolness1@gmail.com>
2025-12-30 14:16:00 -0700

    tweaks

--- server.cjs
+++ server.cjs
@@ -14,13 +14,10 @@ app.use((req, res, next) => {
 });
 
 // Serve _next assets
-// Serve _next assets both at the deployment basePath and root (local testing)
 app.use(`${basePath}/_next`, express.static(path.join(buildPath, '_next')));
-app.use('/_next', express.static(path.join(buildPath, '_next')));
 
-// Serve static files (JS, CSS, etc.) at both the root and the deployment basePath
+// Serve static files (JS, CSS, etc.)
 app.use(basePath, express.static(buildPath));
-app.use('/', express.static(buildPath));
 
 // Fallback route
 app.get(`${basePath}/*`, (req, res) => {
@@ -31,24 +28,10 @@ app.get(`${basePath}/*`, (req, res) => {
     // Try to serve a file directly
     fs.stat(filePath, (err, stats) => {
         if (!err && stats.isFile()) {
-            // If it's an HTML file, rewrite any absolute production URLs to local basePath
-            if (filePath.endsWith('.html')) {
-                fs.readFile(filePath, 'utf8', (readErr, data) => {
-                    if (readErr) return res.sendFile(filePath);
-                    const replaced = data.replace(/https:\/\/projects\.montanafreepress\.org\/capitol-tracker-2025/g, basePath);
-                    res.type('html').send(replaced);
-                });
-            } else {
-                res.sendFile(filePath);
-            }
+            res.sendFile(filePath);
         } else {
-            // Fallback to index.html for client-side routing; rewrite asset URLs for local testing
-            const indexPath = path.join(buildPath, 'index.html');
-            fs.readFile(indexPath, 'utf8', (readErr, data) => {
-                if (readErr) return res.sendFile(indexPath);
-                const replaced = data.replace(/https:\/\/projects\.montanafreepress\.org\/capitol-tracker-2025/g, basePath);
-                res.type('html').send(replaced);
-            });
+            // Fallback to index.html for client-side routing
+            res.sendFile(path.join(buildPath, 'index.html'));
         }
     });
 });
